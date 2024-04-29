7bedb82c024ee19066a82f2210d3d29be8328070
Muhammad Ibad <ibad3572@gmail.com>
2024-04-29 15:27:10 +0500

    Add .env file to .gitignore and create User model

---
+++ src/models/user.model.ts
@@ -0,0 +1,47 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+interface Message extends Document {
+    content: string;
+    createdAt: Date;
+}
+
+const messageSchema: Schema<Message> = new Schema({
+    content: { type: String, required: [true, "Message is required"] },
+    createdAt: { type: Date, default: Date.now, required: true },
+});
+
+interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    messages: Message[];
+    isAcceptingMassages: boolean;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isVerified: boolean;
+}
+
+const userSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        trim: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        match: [/.+\@.+\..+/, "Please fill a valid email address"],
+    },
+    password: { type: String, required: [true, "Password is required"] },
+    messages: [messageSchema],
+    isAcceptingMassages: { type: Boolean, default: true },
+    verifyCode: { type: String },
+    verifyCodeExpiry: { type: Date },
+    isVerified: { type: Boolean, default: false },
+});
+
+export const User =
+    mongoose.model<User>("User", userSchema) ||
+    (mongoose.models.User as mongoose.Model<User>);
