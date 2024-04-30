4c019672884c64dfd5ae5552c51f631e4504b81b
Muhammad Ibad <ibad3572@gmail.com>
2024-05-01 00:37:15 +0500

    Add emailHelper function for sending verification emails

--- src/helpers/emailHelper.ts
+++ src/helpers/emailHelper.ts
@@ -1,7 +1,24 @@
 import { resend } from "@/lib/resend";
 import VerificationEmail from "../../templates/VerificationEmailTemplate";
+import { ApiResponse } from "@/types/ApiResponse";
 
+export async function emailHelper(
+    username:string,
+    email:string,
+    verifyCode:string
+):Promise<ApiResponse>{
+    try {
+        const { data, error } = await resend.emails.send({
+            from: 'Acme <onboarding@resend.dev>',
+            to: email,
+            subject: 'Verification Code',
+            react: VerificationEmail({ username,otp:verifyCode }),
+          });
+        return {success:true, message:"Email send Successfully"}
 
-export defult function emailHelper():Promise<{
+    } catch (error) {
+        console.error("Error while sending an email")
+        return {success:false, message:"Failed to send Email"}
+    }
     
 }
