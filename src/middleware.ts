bbd004d66a8b710e75cfd0c3d4fcc662265a2a5d
Muhammad Ibad <ibad3572@gmail.com>
2024-05-03 01:14:41 +0500

    Refactor middleware to handle authentication and redirection logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,30 @@
-import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-export { default } from "next-auth/middleware";
-
-export async function middleware(request: NextRequest) {
-    const token = await getToken({ req: request });
-    const url = request.nextUrl.pathname;
-
-    if (
-        token &&
-        (url.startsWith("/login") ||
-            url.startsWith("/signup") ||
-            url.startsWith("/verify"))||
-            url.startsWith("/")
-    ) {
-        return NextResponse.redirect(new URL("/dashboard", request.url));
-    }
-    
-}
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+export { default } from 'next-auth/middleware';
 
 export const config = {
-    matcher: ["/login", "/signup", "/", "/verify", "/dashboard/:path*"],
+  matcher: ['/dashboard/:path*', '/login', '/signup', '/', '/verify/:path*'],
 };
+
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
+
+  // Redirect to dashboard if the user is already authenticated
+  // and trying to access sign-in, sign-up, or home page
+  if (
+    token &&
+    (url.pathname.startsWith('/sign-in') ||
+      url.pathname.startsWith('/sign-up') ||
+      url.pathname.startsWith('/verify') ||
+      url.pathname === '/')
+  ) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
+
+  if (!token && url.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/sign-in', request.url));
+  }
+
+  return NextResponse.next();
+}
