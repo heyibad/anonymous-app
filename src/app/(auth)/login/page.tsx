935c8023c26c3c8bec80511a10f72ef55f0d7e01
Muhammad Ibad <ibad3572@gmail.com>
2024-05-03 01:14:54 +0500

    Add AuthProvider component for handling authentication in the app

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+"use client"
 
-const Page = () => {
+import { useSession, signIn, signOut } from "next-auth/react"
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
   return (
-    <div>Page</div>
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
   )
 }
-
-export default Page
