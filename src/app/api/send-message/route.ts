36932dd5a1bd88004e44506ecf21132ecedf4ab9
Muhammad Ibad <ibad3572@gmail.com>
2024-05-06 15:14:07 +0500

    feat: Add API route for sending messages

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,35 @@
+import dbConnect from "@/lib/dbConnect";
+import { User, Message } from "@/models/user.model";
+import { NextRequest } from "next/server";
+
+export async function name(req: NextRequest) {
+    await dbConnect();
+    const { username, content } = await req.json();
+    try {
+        const user = await User.findOne({ username });
+        if (!user) {
+            return Response.json(
+                { message: "User not found", status: false },
+                { status: 404 }
+            );
+        }
+        if (!user.isAcceptingMassages) {
+            return Response.json(
+                { message: "User is not accepting messages", status: false },
+                { status: 400 }
+            );
+        }
+        const message = { content, createdAt: new Date() };
+        user.messages.push(message as Message);
+        await user.save();
+        return Response.json(
+            { message: "Message sent successfully", status: true },
+            { status: 200 }
+        );
+    } catch (error) {
+        return Response.json(
+            { message: "Internal server error", status: false },
+            { status: 500 }
+        );
+    }
+}
