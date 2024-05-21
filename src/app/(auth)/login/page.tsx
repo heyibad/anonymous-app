3c3f68f4622e4b3969b5464c3b0b43d08ddf1d4a
Muhammad Ibad <ibad3572@gmail.com>
2024-05-21 12:32:02 +0500

    feat: Add RootLayout component for wrapping the app layout

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -73,7 +73,7 @@ const Page = () => {
     };
 
     return (
-        <div className=" flex justify-center min-h-screen items-center bg-gray-800">
+        <div className=" flex justify-center min-h-screen items-center bg-gray-800 bg">
             <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
                 <div className="text-center">
                     <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
